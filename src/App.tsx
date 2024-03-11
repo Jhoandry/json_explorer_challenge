622a10dcfb8bf2c161ed5aa4f8fee73d31f8cbd0
Jhoandry Sequera <jsequera@optoro.com>
2024-03-11 12:59:27 +0100

    json-exporter-challenge: add render to show json data as expected and add react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import JsonExplorer from './components/JsonExplorer';
 function App() {
   return (
     <div className="App">
+      <h1>JSON Explorer Challenge</h1>
       <JsonExplorer />
     </div>
   );
--- src/components/JsonExplorer.tsx
+++ src/components/JsonExplorer.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import data from '../assets/data.json';
+import Card from 'react-bootstrap/Card';
 
 type Detail = {
   name: string;
-  value: string;
+  path: string,
+  value: any;
   type: string;
   clickable: boolean;
-  children?: Detail[];
+  isArray: boolean;
+  children: Detail[];
 };
 
 function buildKey(key:string, path:string, isParentArray:boolean) {
@@ -19,20 +22,55 @@ function buildKey(key:string, path:string, isParentArray:boolean) {
 function getDetails(data: object, path:string='', isParentArray:boolean=false): Detail[] {
   return Object.entries(data).map(([key, value]) => {
     let isObject = typeof value === 'object';
-    let keyName = buildKey(key, path, isParentArray);
+    let isArray = Array.isArray(value);
+    let pathKey = buildKey(key, path, isParentArray);
     return {
-      name: keyName,
-      value: value.toString(),
+      name: key,
+      path: pathKey,
+      value: value,
       type: typeof value,
       clickable: !isObject,
-      children: isObject? getDetails(value, keyName, Array.isArray(value)) : []
+      isArray: isArray,
+      children: isObject? getDetails(value, pathKey, isArray) : []
     };
   });
 }
 
+
+function renderDetail(detail:Detail) {
+  if (detail.clickable) {
+    return (
+      <li>
+        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+          { detail.name }
+        </a> : <label> {detail.value} </label>
+      </li>
+    )
+  }
+
+  return (
+    <li>
+      <label> 
+        { detail.isArray? `${detail.name}: [` : '{' }
+          <ul> {detail.children.map(detail => renderDetail(detail))} </ul>
+        { detail.isArray? ']' : '}'  }
+      </label>
+    </li>
+  )
+};
+
 function JsonExplorer () {
   const details = getDetails(data)
-  return <>{details.length}</>;
+  const renderDetails = details.map(detail => renderDetail(detail));
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Response</Card.Title>
+        <Card.Body className="response"> {renderDetails} </Card.Body>
+      </Card.Body>
+    </Card>
+  )
 }
 
 export default JsonExplorer;
